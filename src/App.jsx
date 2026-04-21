01e9732799b431e7bae79328660889ef2d673587
Karen Cassani Patiño <karen@Karens-MacBook-Air.local>
2026-04-21 17:51:23 -0600

    adding comments to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,26 @@ import { useEffect, useState } from 'react';
 function App() {
   const url = 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0';
   const { data, loading, error } = useFetch(url);
-
+/*Estados para los datos compleots */
   const [pokemones, setPokemones] = useState([]);
   const getListPokemones = () => {
+    /*esta funcion va y descarga los detalles, fotos ataques */
     const list = data?.results?.filter((p) => p.url);
+   /*creamos una lista de peticiones fetch*/
     const plist = list?.map((l) => fetch(l.url).then((res) => res.json()));
     Promise.all(plist).then((values) => {
       setPokemones(values);
     });
   };
-
+/*Se ejecuta cuando la data del primer fetch cambia */
   useEffect(() => {
     getListPokemones();
   }, [data]);
 
-  const [position, setPosition] = useState(1);
-  const [myPokeSelection, setMyPokeSelection] = useState([]);
-  const [pcPokeSelection, setPcPokeSelection] = useState([]);
-
+  const [position, setPosition] = useState(1); //El ID del pokemon resaltado en la lista 
+  const [myPokeSelection, setMyPokeSelection] = useState([]); //Mi pokemon elegido 
+  const [pcPokeSelection, setPcPokeSelection] = useState([]); //El pokemon que elige la maquina 
+  /*La logica de movimientos, controlamos los limites para no salirnos del catalogo */
   const handleDirection = (direction) => {
     if (direction === 'right' && position + 1 < 101) {
       setPosition((prev) => prev + 1);
@@ -41,7 +43,7 @@ function App() {
       setPosition(1);
     }
   };
-
+//Genera un numero al azar para que la computadora elija su pokemon 
   function getRandomInt(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
@@ -53,11 +55,11 @@ function App() {
     const pc = pokemones.filter((p) => p.id === rnd);
     setPcPokeSelection(pc);
   }
-
+//Cuando presionas el boton a en right control 
   const handleSelection = () => {
     const selectPokemon = pokemones.filter((p) => p.id === position)
-    setMyPokeSelection(selectPokemon);
-    computerSelection();
+    setMyPokeSelection(selectPokemon); //Guarda mi eleccion 
+    computerSelection(); //Me da la eleccion de la PC
   }
 
   return (
