3231796eebab284ce60e8e735dedd2c663a7818e
Karen Cassani Patiño <karen@Karens-MacBook-Air.local>
2026-04-21 17:36:15 -0600

    adding comments to right control

--- src/components/RightControl.jsx
+++ src/components/RightControl.jsx
@@ -3,21 +3,23 @@ import React from 'react'
 /* Añadimos setMyPokeSelection y setPcPokeSelection para poder reiniciar el juego */
 function RightControl({ handleSelection, setMyPokeSelection, setPcPokeSelection }) {
     
-    // Función para resetear la batalla y volver a la lista
+    // Función para resetear la batalla y volver a la lista, al quedar vacios la logica de mi app principla volvera a mostrar la lista de pokemon
     const handleReset = () => {
         setMyPokeSelection([]);
         setPcPokeSelection([]);
     };
 
     return (
+        /*Contenedor derecho, puse el color, borde redondeado*/
         <div className="w-[100px] h-[280px] border-4 border-white shadow-lg relative bg-[#D4BEE4] rounded-r-[3rem] flex flex-col items-center">
             <div className="absolute -top-4 right-4 w-20 h-5 bg-[#C0A0D3] rounded-t-2xl border-x-4 border-t-4 border-white"></div>
 
             <div className="absolute top-6 left-3 w-6 h-6 flex items-center justify-center bg-[#C0A0D3] rounded-full text-white font-bold shadow-sm cursor-pointer hover:scale-110 transition-transform text-xl">
                 +
             </div>
-
+{/*contenedor de botones de accion X,Y,A,B */}
             <div className="mt-12 w-20 h-20 relative">
+                {/*BOTON X AZUL : solo es estetico*/}
                 <button 
                     className="absolute top-0 left-1/2 -translate-x-1/2 w-7 h-7 bg-[#AEE2FF] border-2 border-white rounded-full flex items-center justify-center shadow-md hover:brightness-90 active:scale-90 transition-all text-white font-extrabold text-xs"
                 >
@@ -31,14 +33,14 @@ function RightControl({ handleSelection, setMyPokeSelection, setPcPokeSelection 
                 >
                     B
                 </button>
-
+{/*BOTON Y VERDE : solo es estetico*/}
                 <button 
                     className="absolute left-0 top-1/2 -translate-y-1/2 w-7 h-7 bg-[#C1E1C1] border-2 border-white rounded-full flex items-center justify-center shadow-md hover:brightness-90 active:scale-90 transition-all text-white font-extrabold text-xs"
                 >
                     Y
                 </button>
 
-                {/* El botón A sigue siendo para SELECCIONAR e iniciar pelea */}
+                {/* El botón A sigue siendo para SELECCIONAR e iniciar pelea es el amarillo */}
                 <button 
                     onClick={handleSelection} 
                     className="absolute right-0 top-1/2 -translate-y-1/2 w-7 h-7 bg-[#FFF5BA] border-2 border-white rounded-full flex items-center justify-center shadow-md hover:brightness-90 active:scale-90 transition-all text-white font-extrabold text-xs"
@@ -46,7 +48,7 @@ function RightControl({ handleSelection, setMyPokeSelection, setPcPokeSelection 
                     A
                 </button>
             </div>
-
+{/*Joystick derecho*/}
             <div className="mt-10 w-16 h-16 bg-[#FDFDFF] rounded-full shadow-inner border-4 border-[#C0A0D3] flex items-center justify-center">
                 <div className="w-12 h-12 bg-white rounded-full shadow-md border-2 border-gray-100"></div>
             </div>
